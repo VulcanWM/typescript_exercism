c16db4b39816b7b65468f4536fda335c4bb43c46
Medea <vulcanwmemail@gmail.com>
2023-03-17 20:37:13 +0000

    Create `triangle.ts`

---
+++ triangle.ts
@@ -0,0 +1,21 @@
+export class Triangle {
+  public sorted:number[];
+  public set:Set<number>;
+  public valid:Boolean;
+  
+  constructor(...sides: number[]) {
+    this.sorted = sides.sort((a,b) => a - b)
+    this.set = new Set(sides);
+    this.valid = this.sorted[0] > 0 && this.sorted[0] + this.sorted[1] >= this.sorted[2]
+  }
+
+  get isEquilateral() {
+    return this.valid && this.set.size === 1
+  }
+  get isIsosceles() {
+    return this.valid && this.set.size <= 2
+  }
+  get isScalene() {
+    return this.valid && this.set.size === 3
+  }
+}
