512cab745128b1292f476a097a1bb7d9ddb923d4
Medea <vulcanwmemail@gmail.com>
2023-03-18 17:08:35 +0000

    Create `grains.ts`

---
+++ grains.ts
@@ -0,0 +1,8 @@
+export function square(s: number): bigint {
+  if (s < 1 || s > 64)
+    throw new Error('Invalid square')
+  return 2n ** BigInt(s - 1)
+}
+export function total(): bigint {
+  return 2n ** 64n - 1n
+}
